171e9f8e5010845d1b55ca6c5e3a52e7dc0bed14
axiel7 <axiel7lopez@gmail.com>
2023-11-03 15:43:36 +0900

    initial staff roles

--- app/src/main/java/com/axiel7/anihyou/data/model/staff/Staff.kt
+++ app/src/main/java/com/axiel7/anihyou/data/model/staff/Staff.kt
@@ -17,3 +17,25 @@ fun StaffDetailsQuery.Staff.yearsActiveFormatted(): String? {
     }
     return null
 }
+
+@Composable
+fun String.staffRoleLocalized() = when (this) {
+    "A&R Producer" -> stringResource(R.string.staff_role_a_and_r_producer)
+    "ADR Director" -> stringResource(R.string.staff_role_adr_director)
+    "Accessory Design" -> stringResource(R.string.staff_role_accessory_design)
+    "Action Animator" -> stringResource(R.string.staff_role_action_animator)
+    "Action Director" -> stringResource(R.string.staff_role_action_director)
+    "Action Animation Director" -> stringResource(R.string.staff_role_action_animation_director)
+    "Action Animation Supervisor" -> stringResource(R.string.staff_role_action_animation_supervisor)
+    "Advertising" -> stringResource(R.string.staff_role_advertising)
+    "Advertising Producer" -> stringResource(R.string.staff_role_advertising_producer)
+    "Animation Director" -> stringResource(R.string.staff_role_animation_director)
+    "Animation Producer" -> stringResource(R.string.staff_role_animation_producer)
+    "Animation Production Assistance" -> stringResource(R.string.staff_role_animation_production_assistance)
+    "Animation Supervisor" -> stringResource(R.string.staff_role_animation_supervisor)
+    "Animation" -> stringResource(R.string.staff_role_animation)
+    "Art" -> stringResource(R.string.staff_role_art)
+    "Art Board" -> stringResource(R.string.staff_role_art_board)
+    "Art Composition" -> stringResource(R.string.staff_role_accessory_design)
+    else -> this
+}
